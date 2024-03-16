41b8d8d9c850fdb165427d1d6cfc017bff542320
s28111 <s28111@pjwstk.edu.pl>
2024-03-16 19:25:53 +0100

    Dodano sout 54321

--- Program.java
+++ Program.java
@@ -5,14 +5,15 @@ public class Program
         int[] ints = new int[]{1,2,3,4,5};
         double srednia = srednia(ints);
         System.out.println(srednia);
+         System.out.println("54321");
     }
     public static double srednia(int[] ints)
     {
         double srednia = 0;
         for(int i = 0; i < ints.lenght; i++)
         {
-            srednia2 += ints[i]/ints.lenght;
+            srednia += ints[i]/ints.lenght;
         }
-        return srednia2;
+        return srednia;
     }
 }
