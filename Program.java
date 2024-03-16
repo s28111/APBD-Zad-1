96a79c72841408c6bdcdc32c62da3418b6e63826
s28111 <s28111@pjwstk.edu.pl>
2024-03-16 18:20:44 +0100

    Dodano metode srednia

---
+++ Program.java
@@ -0,0 +1,17 @@
+public class Program
+{
+    public static void main(String[] args)
+    {
+        int[] ints = new int[]{1,2,3,4,5};
+        double srednia = srednia(ints);
+    }
+    public static double srednia(int[] ints)
+    {
+        double srednia = 0;
+        for(int i = 0; i < ints.lenght; i++)
+        {
+            srednia += ints[i]/ints.lenght;
+        }
+        return srednia;
+    }
+}
